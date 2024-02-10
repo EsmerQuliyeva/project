02dfe4e59e8a3d5a8c4418ac263c7ad732c1bf2c
Esmer Quliyeva <139906367+EsmerQuliyeva@users.noreply.github.com>
2024-02-10 04:41:19 -0800

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+
+
+const Navbar = () => {
+    return (
+        <div className='navbar_container'>
+            <div className='logo'>
+                AskExperts
+            </div>
+            <div style={{'width': '100 px', 'height': '30px', 'color':'green'}}>Home</div>
+            <div>Pages</div>
+            <div>Service</div>
+            <div>Work</div>
+            <div>Pages</div>
+            <div>Contact</div>
+            <div className='number'>
+                <p>+(888)452 1505</p>
+            </div>
+        </div >
+
+    )
+}
+
+
+
+export default Navbar
